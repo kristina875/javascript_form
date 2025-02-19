5030e056471cbb67bd61f77b78edac1fc5582672
ksanchez23-0337 <ksanchez23-0337@cca.edu.ph>
2025-02-19 18:40:55 +0800

    action
    new file

---
+++ table.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> table form </title>
+    </head>
+    <body>
+
+        <h1> Student Record </h1>
+        <table id=""student_record>
+        </table>
+        
+    </body>
+</html>
