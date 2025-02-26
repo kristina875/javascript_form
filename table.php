ffe1b3d90c6277cc555c5655d813d0197fb4e3ed
kristina875 <ksanchez23-0337@cca.edu.ph>
2025-02-26 17:56:41 +0800

    Update table.php

--- table.php
+++ table.php
@@ -4,37 +4,49 @@
     </head>
     <body>
 
-        <h1> Student Record </h1>
-        <table id="student_record" border ="1">
+         <h1>Student Record</h1>
+    <table id="Student_Record" border="1">
 
-        </table>
-        <script>
-            const studentRecord=document.getElementById("student_record");
-            
-            console.log("hello world");
-            const row1 = studentRecord.insertRow();
-            const row1col1 = row1.insertcell();
-            row1col1.innerHTML ="Last Name";
+    </table>
 
-            const row1col2 = row1.insertCell();
-            row1col2.innerHTML = "First Name";
+    <script>
+        const StudentRecord = document.getElementById("Student_Record");
 
-            const row2 = studentRecord.insertRow();
-            const row2col1 = row2.insertcell();
-            const row2col2 = row2.insertCell();
+        console.log("Hello World");
 
-            const inputLastName = document.createElement("INPUT");
-            inputLastName.type = "text";
-            inputLastName.placeholder = "Enter Last Name"
+        const row1 = StudentRecord.insertRow();
 
-            row2col1.append(inputLastName);
-            row2col2.append(inputFirstName);
-            
-            const row3 = studentRecord.insertRow();
-            const row3col = row3.insertCell();
-            row3col.append(SubmitButton);
+        const row1col1 = row1.insertCell();
+        row1col1.innerHTML = "Last Name";
 
-            console.log(studentRecord);
-        </script>
+        const row1col2 = row1.insertCell();
+        row1col2.innerHTML = "First Name";
+
+        const row2 = StudentRecord.insertRow();
+        const row2col1 = row2.insertCell();
+        const row2col2 = row2.insertCell();
+
+        const inputLastName = document.createElement("INPUT");
+        inputLastName.type = "text";
+        inputLastName.placeholder = "Enter Last Name";
+
+        row2col1.append (inputLastName);
+
+        const inputFirstName = document.createElement("INPUT");
+        inputFirstName.type = "text";
+        inputFirstName.placeholder = "Enter Last Name";
+
+        row2col2.append (inputFirstName);
+
+        const row3 = StudentRecord.insertRow();
+        const row3col1 = row3.insertCell();
+        row3col1.colSpan = 2;
+
+        const submitButton = document.createElement("INPUT");
+        submitButton.type = "submit";
+        submitButton.value = "Submit Record";
+        row3col1.append(submitButton);
+        console.log(StudentRecord);
+    </script>
     </body>
 </html>
